201640916fefb4c4c6ba8ab496fa3f1745508a74
Devyani Jinturkar <work.with.devyani@gmail.com>
2026-08-28 20:16:57 +0530

    Use overview highlight reel (Video 1 + Video 2) in hero showcase

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ document.querySelectorAll('.nav-links a[href^="#"]').forEach(function (link) {
     document.getElementById("navLinks").classList.remove("open");
   });
 });
+
+var heroShowreel = document.getElementById("heroShowreel");
+if (heroShowreel) {
+  var heroClips = ["Content/1_Video 1.mp4", "Content/2_Video 2.mp4"];
+  var heroClipIndex = 0;
+  heroShowreel.addEventListener("ended", function () {
+    heroClipIndex = (heroClipIndex + 1) % heroClips.length;
+    heroShowreel.src = heroClips[heroClipIndex];
+    heroShowreel.play();
+  });
+}
